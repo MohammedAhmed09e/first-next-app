579ba8fd458deeeed8bc4c3ab9875933e18a1227
MohammedAhmed09e <medobashamaser@gmail.com>
2023-11-20 15:27:48 +0200

    first-add

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-       <h1>Welcome</h1>
+       <Link href={"nav"}><h1>Welcome</h1> </Link >
       </div>
     </main>
   )
 }
+ 
